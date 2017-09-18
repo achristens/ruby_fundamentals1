3a5f3ad2c27073962a431ce7c2eec00fb983888b
Abby Christens <achristens@gmail.com>
2017-09-18 15:45:02 -0400

    Allows user to have a snack to regain energy

--- exercise5.rb
+++ exercise5.rb
@@ -16,13 +16,18 @@ while user_distance >= 0
       break
   end
   while energy <= 0
-    puts "Slow down there! You're out of energy. Type walk to keep going, or type go home to exit"
+    puts "Slow down there! You're out of energy. Here are your options. Type:\nwalk to keep going\neat to have a snack\nrest to take a break\ngo home to exit"
     user_input = gets.chomp
       if user_input == "walk" || user_input =="Walk"
         user_distance += 1
         energy += 1
         puts "Distance from home is #{user_distance}km. Your energy level is #{energy}%."
-
+      elsif user_input == "rest" || user_input == "Rest"
+        energy += 10
+        puts "Distance from home is #{user_distance}km. Your energy level is #{energy}%."
+      elsif user_input == "eat" || user_input == "Eat"
+        energy += 5
+        puts "Distance from home is #{user_distance}km. Your energy level is #{energy}%."
       else user_input == "go home" || user_input == "run"
         break
   end
