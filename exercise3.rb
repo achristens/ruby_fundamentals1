9451bf44f133c0e3f42d8148ea3db88221786e64
Abby Christens <achristens@gmail.com>
2017-09-18 13:43:19 -0400

    Calculating age of user

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 user_name = gets.chomp
 puts "Hello, #{user_name}"
+puts "How old are you?"
+user_age = gets.chomp
+puts "Cool, you must have been born in #{2017 - user_age.to_i}"
