1110307efc4277d1b1df2b4946b105e14ed7a336
Abby Christens <achristens@gmail.com>
2017-09-18 15:39:59 -0400

    Alerts users if their uenergy dips below 0 and are unable to run

--- exercise5.rb
+++ exercise5.rb
@@ -1,15 +1,30 @@
 user_distance = 0
+energy = 50
 
 while user_distance >= 0
   puts "Would you like to walk or run?"
   user_input = gets.chomp
     if user_input == "walk" || user_input =="Walk"
       user_distance += 1
-      puts "Distance from home is #{user_distance}km."
-    elsif user_input == "run" || user_input =="Run"
+      energy += 1
+      puts "Distance from home is #{user_distance}km. Your energy level is #{energy}%."
+    elsif (user_input == "run" || user_input =="Run") && energy > 0
       user_distance += 5
-      puts "Distance from home is #{user_distance}km."
+      energy -= 5
+      puts "Distance from home is #{user_distance}km.Your energy level is #{energy}%."
     else user_input == "go home"
       break
-    end
+  end
+  while energy <= 0
+    puts "Slow down there! You're out of energy. Type walk to keep going, or type go home to exit"
+    user_input = gets.chomp
+      if user_input == "walk" || user_input =="Walk"
+        user_distance += 1
+        energy += 1
+        puts "Distance from home is #{user_distance}km. Your energy level is #{energy}%."
+
+      else user_input == "go home" || user_input == "run"
+        break
+  end
+  end
 end
