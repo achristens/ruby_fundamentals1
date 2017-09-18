76fe3e124921ea39fbc3be344cb9bc4521b79189
Abby Christens <achristens@gmail.com>
2017-09-18 15:17:27 -0400

    Allows user to go home and stop the loop

--- exercise5.rb
+++ exercise5.rb
@@ -2,12 +2,14 @@ user_distance = 0
 
 while user_distance >= 0
   puts "Would you like to walk or run?"
-  user_movement = gets.chomp
-if user_movement == "walk" || user_movement =="Walk"
-  user_distance += 1
-  puts "Distance from home is #{user_distance}km."
-else user_movement == "run" || user_movement =="Run"
-  user_distance += 5
-  puts "Distance from home is #{user_distance}km."
-end
+  user_input = gets.chomp
+    if user_input == "walk" || user_input =="Walk"
+      user_distance += 1
+      puts "Distance from home is #{user_distance}km."
+    elsif user_input == "run" || user_input =="Run"
+      user_distance += 5
+      puts "Distance from home is #{user_distance}km."
+    else user_input == "go home"
+      break
+    end
 end
